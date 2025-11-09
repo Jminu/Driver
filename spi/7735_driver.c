e362d11415ed1c976d2fdf759d82974b70e53969
Jin Minu <s9430939@naver.com>
2025-11-09 23:31:22 +0900

    gpio핀 가져오기

--- spi/7735_driver.c
+++ spi/7735_driver.c
@@ -3,9 +3,31 @@
 #include <linux/module.h>
 #include <linux/spi/spi.h>
 #include <linux/of.h>
+#include <linux/delay.h>
+#include <linux/gpio/consumer.h>
+
+static struct st7735_priv {
+    struct spi_device *spi;
+    struct gpio_desc *reset; // BCM 22
+    struct gpio_desc *dc; // BCM 17
+    struct gpio_desc *bl; // BCM 27
+};
 
 // spi드라이버를 디바이스에 바인딩
 static int st7735_custom_probe(struct spi_device *spi) {
+    struct device *dev = &spi->dev;
+    struct st7735_priv *priv;
+    int ret;
+
+    priv = devm_kzalloc(dev, sizeof(struct st7735_priv), GFP_KERNEL);
+    priv->spi = spi;
+
+    priv->reset = devm_gpiod_get(dev, "reset", GPIOD_OUT_HIGH);
+    priv->dc = devm_gipod_get(dev, "dc", GPIOD_OUT_HIGH);
+    priv->bl = devm_gpiod_get(dev, "bl", GPIOD_OUT_HIGH);
+
+    spi_set_drvdata(spi, priv);
+    
     printk(KERN_INFO "Probe func success\n");
 
     return 0;
